fdeda4f30aa9877efa0bcde6d15aa00e573e87f1
고영욱 <gyw8526@naver.com>
2020-08-10 12:31:53 +0900

    d

--- backend/src/main/java/com/web/blog/controller/account/CommentController.java
+++ backend/src/main/java/com/web/blog/controller/account/CommentController.java
@@ -34,22 +34,24 @@ public List<Comment> detailComment(@PathVariable String uid, @PathVariable Strin
     ArrayList<Comment> rlist = new ArrayList<>();
     System.out.println(list.size());
     for (int i = 0; i < list.size(); i++) {
-        if(list.get(i).getBoardIdx() == Integer.parseInt(id)) { 
+        if(list.get(i).getBoardIdx() == Integer.parseInt(id)) {
             System.out.println(list.get(i).toString());
-            rlist.add(list.get(i));
-        } 
+            rlist.add(new Comment(list.get(i).getIdx(), list.get(i).getBoardIdx(), list.get(i).getContent(), list.get(i).getWriter(), null, null, null, null));
+        }
     }
     System.out.println(rlist.size() + " 댓글개수");
     return rlist;
 }
 
- 
+
 @ApiOperation(value = "새로운 댓글 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 @PostMapping("list/detail/comments/{id}/write")
 public Comment writeComment(@RequestBody Comment comment ,@PathVariable("id") int id) {
     //board = new Board(0, "test", "test", null, 0, "unknown", 300);
-//    comment.setBoardIdx(id);
-    return commentDao.save(comment);
+    
+	comment.setBoardIdx(id);
+    
+	return commentDao.save(comment); 
 }
 
 @ApiOperation(value = "게시글번호에 해당하는 게시글의 정보를 삭제한다.", response = String.class)    
--- backend/src/main/java/com/web/blog/dao/user/CommentDao.java
+++ backend/src/main/java/com/web/blog/dao/user/CommentDao.java
@@ -11,3 +11,4 @@ import com.web.blog.model.user.Comment;
 public interface CommentDao extends JpaRepository<Comment, Integer>{
 	List<Comment> findByBoardIdx(String bid);
 }
+ 
