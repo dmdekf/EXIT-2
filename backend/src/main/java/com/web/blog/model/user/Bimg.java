eb0e326f8a4156583605a21d3225355173137385
전소윤 <twoposition@naver.com>
2020-08-13 16:35:03 +0900

    BoardImgage Backend

---
+++ backend/src/main/java/com/web/blog/model/user/Bimg.java
@@ -0,0 +1,23 @@
+// 하단 DB 설정 부분은 Sub PJT II에서 데이터베이스를 구성한 이후에 주석을 해제하여 사용.
+
+package com.web.blog.model.user;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class  Bimg{
+	
+	@Id 
+	private int id;
+	
+	private int bid;
+	private String uimage;
+}
